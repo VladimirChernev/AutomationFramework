bc179e4f3efe838b54fb07402d14d95c96c108b1
VladimirChernev <vchernev@abv.bg>
2017-10-13 21:32:53 +0300

    Added system property for the gecko web driver

--- src/utils/Browser.java
+++ src/utils/Browser.java
@@ -8,8 +8,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Browser {
 
 	public static WebDriver driver;
-	
+
 	public static void init() {
+
+		// geckodriver-v0.19.0-win64.zip
+		System.setProperty("webdriver.gecko.driver", "C:/Zip/Docs/Work/Java Dev Course/geckodriver.exe");
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
